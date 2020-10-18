c038ed6e852b9f2f9f2fa45bd96b3132a67603d2
khatabwedaa <khatabwedaa@gmail.com>
2020-10-18 17:42:08 +0200

    Update install

--- install.sh
+++ install.sh
@@ -2,10 +2,21 @@
 
 echo "Setting up your PC..."
 
+# Set default MySQL root password and auth type.
+mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+
+# Install global Composer packages
+composer global require laravel/installer cpriego/valet-linux
+
+# Install Laravel Valet
+valet install
+
 # Create Code directory
 mkdir $HOME/Code
 
+# Clone Github repositories
 ./clone.sh
 
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc

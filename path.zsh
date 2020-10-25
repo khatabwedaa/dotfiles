28fb126f765f61086b3755bfff6bf58a5dfc5df2
khatabwedaa <khatabwedaa@gmail.com>
2020-10-25 12:15:34 +0200

    Add nvm path

--- path.zsh
+++ path.zsh
@@ -3,3 +3,7 @@ export PATH="$HOME/.config/composer/vendor/bin:$PATH"
 
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion

4fe309a09df03d4c3f2eaf70b2f63c9142e3343b
khatabwedaa <khatabwedaa@gmail.com>
2020-10-25 12:17:45 +0200

    Add composer installation

--- install.sh
+++ install.sh
@@ -5,6 +5,23 @@ echo "Setting up your PC..."
 # Set default MySQL root password and auth type.
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
+# Install Composer
+EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+exit $RESULT
+
 # Install global Composer packages
 composer global require laravel/installer cpriego/valet-linux
 

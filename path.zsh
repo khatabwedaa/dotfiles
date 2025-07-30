34a939ac159f26358f00e2f8d7e35241e2ca3290
Alkhatab Wedatalla <a.wedatalla@nearpay.io>
2025-07-30 17:56:08 +0300

    Update path.zsh to set JAVA_HOME and ANDROID_HOME, and adjust NVM_DIR path

--- path.zsh
+++ path.zsh
@@ -7,9 +7,14 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 
 export PATH=/opt/homebrew/bin:$PATH
 
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
-source $(brew --prefix nvm)/nvm.sh
-export NVM_DIR=~/.nvm
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion

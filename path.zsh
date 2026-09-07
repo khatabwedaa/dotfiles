3245008a82397075a2d37c7e756afc51d8806a5a
khatabwedaa <khatabwedaa@gmail.com>
2026-09-07 14:51:42 +0300

    Refactor nvm configuration in path.zsh to use Homebrew installation

--- path.zsh
+++ path.zsh
@@ -18,9 +18,11 @@ export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PNPM_HOME="$HOME/Library/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 
-# source $(brew --prefix nvm)/nvm.sh
-export NVM_DIR=~/.nvm
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# nvm (installed via Homebrew: brew install nvm)
+export NVM_DIR="$HOME/.nvm"
+NVM_HOMEBREW="${HOMEBREW_PREFIX:-/opt/homebrew}/opt/nvm"
+[ -s "$NVM_HOMEBREW/nvm.sh" ] && \. "$NVM_HOMEBREW/nvm.sh"                                   # This loads nvm
+[ -s "$NVM_HOMEBREW/etc/bash_completion.d/nvm" ] && \. "$NVM_HOMEBREW/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+unset NVM_HOMEBREW
 
 export PATH="$HOME/.local/bin:$PATH"
